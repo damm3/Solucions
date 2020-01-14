3d4c43e740972caa9c7d36659f0f35970ce55582
gerard <gerardfp@gmail.com>
2020-01-14 22:16:09 +0100

    -

--- src/com/company/c9ac3.java
+++ src/com/company/c9ac3.java
@@ -5,10 +5,13 @@ import java.util.Scanner;
 public class c9ac3 {
 
     /*
-        Los algoritmos de ordenación mas estudiados suelen ser:
+        Los algoritmos de ordenacion mas estudiados suelen ser:
             * Bubblesort
             * Mergesort
             * Quiksort
+
+            Para este ejercicio implementamos el Bubblesort, ya que es
+            el mas sencillo de los tres.
      */
 
     public static void main(String[] args) {
@@ -21,17 +24,30 @@ public class c9ac3 {
             seq[i] = scanner.nextInt();
         }
 
-        boolean intercambio = false;
-        while(!intercambio) {
+        boolean intercambio;
+        do {
              intercambio = false;
 
             for (int i = 1; i < n; i++) {
-                if (seq[i-1] < seq[i]){
+                if (seq[i-1] > seq[i]){
+                    // intercambiamos los valores que hay en las
+                    // posiciones [i-1] y [i]
 
+                    int v = seq[i-1];
+                    seq[i-1] = seq[i];
+                    seq[i] = v;
+                    intercambio = true;
                 }
             }
-        };
+        } while(intercambio);
+        // cuando ya no se haya hecho ningun intercambio
+        // es porque todos los valores del array ya estaban
+        // ordenados
 
 
+        // mostramos el array
+        for (int i = 0; i < n; i++) {
+            System.out.print(seq[i] + " ");
+        }
     }
 }
