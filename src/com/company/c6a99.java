03e0b8689b54ab2934081bb91bec580506385ba3
gerard <gerard>
2020-01-28 16:43:01 +0100

    -

---
+++ src/com/company/c6a99.java
@@ -0,0 +1,44 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class c6a99 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // 68 comptadors, un per a cada cromo
+        int[] repes = new int[68];
+
+        int n = scanner.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int id = scanner.nextInt();
+
+            repes[id-1]++;
+        }
+
+        for (int i = 0; i < 68; i++) {
+            if(repes[i] > 1){
+                System.out.println((i+1) + ": " + repes[i]);
+            }
+        }
+    }
+}
+
+/*
+5
+11 3 9 11 5
+
+5
+11 33 11 11 33
+
+5
+1 2 3 4 5
+
+9
+1 2 3 1 2 3 1 2 3
+
+100
+9 25 57 61 37 4 26 44 29 61 18 66 33 24 20 20 2 51 4 55 44 32 31 21 7 51 52 68 43 27 60 9 42 23 23 47 26 24 50 52 60 26 5 24 23 55 43 44 35 6 30 22 18 1 51 29 34 36 35 10 56 54 66 1 62 29 60 63 35 1 5 5 13 40 20 21 62 18 35 35 12 38 23 51 59 44 68 8 51 55 60 64 20 34 64 21 13 48 66 57
+ */
